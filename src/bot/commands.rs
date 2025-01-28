c5ac246c7dd0920c8f6a762e75f2c949429e3e1f
Zubair <zubair.maqsood6@gmail.com>
2025-01-28 01:33:29 +0000

    commands errors

--- src/bot/commands.rs
+++ src/bot/commands.rs
@@ -1,16 +1,16 @@
 use teloxide::utils::command::BotCommands;
 
 #[derive(BotCommands, Clone)]
-#[BotCommands(rename_rule = "lowercase")]
+#[command(rename_rule = "lowercase")]
 pub enum Command {
-    #[BotCommands(description = "Start tracking whales")]
+    #[command(description = "Start tracking whales")]
     Start,
-    #[BotCommands(description = "Stop tracking whales")]
+    #[command(description = "Stop tracking whales")]
     Stop,
-    #[BotCommands(description = "Set minimum transaction amount (in SOL)")]
+    #[command(description = "Set minimum transaction amount (in SOL)")]
     SetMinimum { amount: f64 },
-    #[BotCommands(description = "Show current tracking settings")]
+    #[command(description = "Show current tracking settings")]
     Settings,
-    #[BotCommands(description = "Show help message")]
+    #[command(description = "Show help message")]
     Help,
 }
